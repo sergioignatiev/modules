659835ac7adb11bc8f203954a00e93280e6ab9ec
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-07-02 17:00:16 +0300

    this

--- scripts.js
+++ scripts.js
@@ -5,12 +5,13 @@ function base(x) {
     return `<h1>Hello ${x} </h1>  `
 }
 function sw(){
-    let inp=document.getElementById("inp")
-    let one =`<input id="inp"/><button onclick="sw()">SaW</button> `
+    let inp=document.getElementById("inp");
+    let int=document.getElementById("in");
+    let one =`<input id="inp"/><input id="in"/><button onclick="sw()">SaW</button> `
     
    if(vis==true){vis=false}
    else if(vis==false){vis=true}
-   let cont=(vis==true)? one:base(vis);
+   let cont=(vis==true)? one:base(int.value);
 
 
 
