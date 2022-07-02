d844075324965e2bc0e686181172003b62f8d1f3
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-07-02 16:12:49 +0300

    j

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
-import {one,two} from './message.js'
+import {one,two} from './message.js';
+let vis=true;
+function sw(){
+   if(vis==true){vis=false}
+   else if(vis==false){vis=true}
+}
+
+
+
 document.getElementById("demo").innerHTML=one("hi")+two("I")
