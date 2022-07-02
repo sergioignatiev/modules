bd4c0b21d77d3725988dc5c21e681759d12fc4f4
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-07-02 10:36:05 +0300

    mm

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
-let modules ="Here it works"
-
-export default modules;
+const modules = () => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + 'years old.';
+  };
+  
+  export default modules;
