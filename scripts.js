329916570497adac30c3779e8b7e22ffcbf6af73
w3schools-test <87079592+sergioignatev@users.noreply.github.com>
2022-07-02 16:16:49 +0300

    switcj

--- scripts.js
+++ scripts.js
@@ -3,8 +3,10 @@ let vis=true;
 function sw(){
    if(vis==true){vis=false}
    else if(vis==false){vis=true}
-}
+   let cont=(vis==true)? one:two;
 
 
 
-document.getElementById("demo").innerHTML=one("hi")+two("I")
+
+document.getElementById("demo").innerHTML=cont}
+sw()
