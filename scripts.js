b99ae7e83f3feeb752ebb3e9da7c5bba1af68eab
sergioignatev <87079592+sergioignatev@users.noreply.github.com>
2022-08-27 22:43:37 +0300

    Update scripts.js
    ss

--- scripts.js
+++ scripts.js
@@ -1,20 +1,2 @@
-
-
-let vis=false;
-function base(x) {
-    return `<h1>Hello ${x} </h1>  `
-}
-function sw(){
-    let inp=document.getElementById("inp");
-    let int=document.getElementById("in");
-    let one =`<input id="inp"/><input id="in"/><button onclick="sw()">SaW</button> `
-    
-   if(vis==true){vis=false}
-   else if(vis==false){vis=true}
-   let cont=(vis==true)? one:base(int.value);
-
-
-
-document.getElementById("demo").innerHTML=cont;
-document.getElementById("img").src=inp.value}
-sw()
+import employees from './test.js'
+document.getElementById("demo").innerHTML=employees
